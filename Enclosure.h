c54fe6da74439e10b1164cc09e85daeee3fb1101
ZiadHegazy16002572 <ziad.2006.hegazy@gmail.com>
2025-11-12 15:22:20 +0200

    Update Enclosure.h

--- Enclosure.h
+++ Enclosure.h
@@ -1,15 +1,24 @@
-#ifndef ENCLOSURE_H
-#define ENCLOSURE_H
+#ifndef Enclosure_H
+#define Enclosure_H
+#include <string>
+using namespace std;
 
-#include "Animal.h"
+class Enclosure{
+    private :
+    virtual [] Animal*;
+    int capacity;
+    int currentCount;
 
-//define enclosure class here 
-
-
-
-
-
-
-
-
+    public :
+        void setcapacity();
+        void setcurrentCount();
+        int getcapacity(int capacity)const;
+        int getcurrentCount(int currentCount)const;
+        Enclosure();
+        Enclosure(int currentCount , int capacity ,virtual [] Animal*);
+        ~Enclosure();
+        void addAnimal(Animal*a)const;
+        void displayAnimals()const;
+    
+};
 #endif
