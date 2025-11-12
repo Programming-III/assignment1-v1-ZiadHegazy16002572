0a27279d34e4a5795c793df414d68693449c29e0
ZiadHegazy16002572 <ziad.2006.hegazy@gmail.com>
2025-11-12 15:26:16 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,16 +1,50 @@
-#include "Animal.h"
-#include "Bird.h"
-#include "Mammal.h"
-#include "Reptile.h"
 #include "Enclosure.h"
+#include "Animal.h"
 #include "Visitor.h"
-#include <iostream>
+#include "Bird.h"
+#include "Reptile.h"
+#include "mamm"
+#include <string>
 using namespace std;
 
+Animal::Animal(){
+    name = "";
+    age = 0;
+    isHungry = false;
+}
+Animal::Animal(string name,int age ,bool isHungry){
+    name = n; 
+    age = a;
+    isHungery= q;
+}
+Animal::~Animal(){};
 
-// ============== MAIN FUNCTION ==============
-int main() {
-    
-    
-    return 0;
+Mammal::Mammal(){
+    furColor="";
+}
+Mammal::Mammal(string name,int age,bool isHungry,string furColor) : Animal(string name,int age ,bool isHungry) {
+ furColor= f;
+}
+Mammel::~Mammel(){};
+
+Bird::Bird(){
+    wingSpan= 0.0;
+}
+Bird::Bird(string name,int age,bool isHungry,float wingSpan) : Animal(string name,int age ,bool isHungry){
+    wingSpan= ws;
+}
+Bird::~Bird(){};
+
+Reptile::Reptile(){
+    isVenomous= false;
+}
+Reptile::Reptile(string name,int age,bool isHungry,bool isVenomous) : Animal(string name,int age ,bool isHungry){
+    isVenomous= v;
+}
+Reptile::~Reptile(){};
+int main(){
+
+
+
+return 0;
 }
