d67c3ed55bf60710b4581155a82dc34063f63b9a
ZiadHegazy16002572 <ziad.2006.hegazy@gmail.com>
2025-11-12 15:22:02 +0200

    Update Bird.h

--- Bird.h
+++ Bird.h
@@ -1,14 +1,17 @@
-#ifndef BIRD_H
-#define BIRD_H
-
+#ifndef Bird_H
+#define Bird_H
 #include "Animal.h"
+#include <string>
+using namespace std;
 
-//define class bird here 
-
-
-
-
-
-
-
+class Bird : public Animal {
+    private:
+      float wingSpan;
+    public:
+    Bird();
+    Bird(string name,int age,bool isHungry,float wingSpan) : Animal(string name,int age ,bool isHungry) ;
+    ~Bird();
+    void setwingSpan();
+    string getwingSpan(float wingSpan)const;
+};
 #endif
