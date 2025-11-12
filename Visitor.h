d4f1c7e3df21e4adaa51f20c099cced7213d0c75
ZiadHegazy16002572 <ziad.2006.hegazy@gmail.com>
2025-11-12 15:23:31 +0200

    Update Visitor.h

--- Visitor.h
+++ Visitor.h
@@ -1,15 +1,23 @@
-#ifndef VISITOR_H
-#define VISITOR_H
-
+#ifndef Visitor_H
+#define Visitor_H
 #include <string>
 using namespace std;
 
-//define visitor class here
+class Visitor{
+    private:
+    string visitorName;
+    int ticketBought;
+    public:
+    Visitor();
+    Visitor(string visitorName , int ticketBought);
+    ~Visitor();
 
+    void setvisitorName();
+    void setticketBought();
 
+    string getvisitorName(string visitorName)const;
+    int getticketBought(int ticketBought)const;
 
-
-
-
-
+    void displayInfo()const;
+};
 #endif
